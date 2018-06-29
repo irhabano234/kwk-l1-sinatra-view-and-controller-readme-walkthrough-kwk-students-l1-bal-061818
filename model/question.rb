f5fcdc2f4f5ff657d627f5cd01506bf2ba6aa03e
irhabano234 <irha.bano@gmail.com>
2018-06-29 13:53:15 +0000

    automatically backed up by learn

--- model/question.rb
+++ model/question.rb
@@ -6,13 +6,13 @@ $balanced=["D","H"]
 
 
 def skin_determinant(user_input)
-  if $oily.include? user_input
+  if $oily.include user_input
     "oily"
-  elsif $dry.include? user_input
+  elsif $dry.include user_input
     "dry"
-  elsif $balanced.include? user_input
+  elsif $balanced.include user_input
     "balanced"
-  elsif $combinational.include? user_input
+  elsif $combinational.include user_input
     "combinational"
 end
 end 
