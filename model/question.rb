ccc4586a113f20fdccbdaa8dd79027bf0590df80
irhabano234 <irha.bano@gmail.com>
2018-06-29 13:42:48 +0000

    automatically backed up by learn

--- model/question.rb
+++ model/question.rb
@@ -1,18 +1,17 @@
-oily=[["A","E"],["D","E"]]
-combinational=[["A","F"], ["A","G"], ["A","H"],
+$oily=[["A","E"],["D","E"]]
+$combinational=[["A","F"], ["A","G"], ["A","H"],
 ["B","E"],["B","F"],["B","H"], ["B","G"], ["C", "E"], ["C", "H"], ["C","G"] ["D","G"]]
-dry=[["C","F"],["D","F"]]
-balanced=["D","H"]
+$dry=[["C","F"],["D","F"]]
+$balanced=["D","H"]
 
 
 def skin_determinant(user_input)
-  if oily.include? user_input
+  if $oily.include? user_input
     "oily"
-  elsif dry.include? user_input
+  elsif $dry.include? user_input
     "dry"
-  elsif balanced.include? user_input
+  elsif $balanced.include? user_input
     "balanced"
-  elsif combinational.include? user_input
+  elsif $combinational.include? user_input
     "combinational"
-end
 end
